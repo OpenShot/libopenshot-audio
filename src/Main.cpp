c0e605fafed2095208d7daeb37dd50e90499b8d5
Jonathan Thomas <Jonathan.Oomph@gmail.com>
2012-06-15 17:45:27 -0500

    initial files in the openshot audio library

---
+++ src/Main.cpp
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a Juce application.
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+int main()
+{
+    // ..your code goes here!
+	int a = 0;
+
+    return a;
+}
