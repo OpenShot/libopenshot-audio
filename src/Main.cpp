417648fb9ee2b001aeb5a14a067375031ac32374
Jonathan Thomas <jonathan@openshot.org>
2014-07-10 14:07:07 -0500

    Updated licensing to be AGPLv3, which is compatible with JUCE.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,30 @@
 /**
- * \file
- * \brief Source code to play a test sound
- * \author Copyright (c) 2011 Jonathan Thomas
- */
-
-/**
- * \mainpage OpenShot Audio Library C++ API
+ * @file
+ * @brief Source code to play a test sound
+ * @author Jonathan Thomas <jonathan@openshot.org>
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2008-2014 OpenShot Studios, LLC
+ * <http://www.openshotstudios.com/>. This file is part of OpenShot Audio
+ * Library (libopenshot-audio), an open-source project dedicated to delivering
+ * high quality video editing and animation solutions to the world. For more
+ * information visit <http://www.openshot.org/>.
+ *
+ * OpenShot Audio Library (libopenshot-audio) is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version
+ * 3 of the License, or (at your option) any later version.
+ *
+ * OpenShot Audio Library (libopenshot-audio) is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenShot Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @mainpage OpenShot Audio Library C++ API
  *
  * Welcome to the OpenShot Audio Library C++ API.  This library is used by libopenshot to enable audio
  * features, which powers the <a href="http://www.openshot.org">OpenShot Video Editor</a> application.
