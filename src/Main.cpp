2bab8ff88cc91894aeeb31f644e2fef2bf489d01
Jonathan Thomas <jonathan@openshot.org>
2014-07-15 13:24:12 -0500

    Changed licensing to GPLv3, to be compatible with JUCE source code.

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,20 +8,20 @@
  * Copyright (c) 2008-2014 OpenShot Studios, LLC
  * <http://www.openshotstudios.com/>. This file is part of OpenShot Audio
  * Library (libopenshot-audio), an open-source project dedicated to delivering
- * high quality video editing and animation solutions to the world. For more
+ * high quality audio editing and playback solutions to the world. For more
  * information visit <http://www.openshot.org/>.
  *
  * OpenShot Audio Library (libopenshot-audio) is free software: you can
- * redistribute it and/or modify it under the terms of the GNU Affero General
+ * redistribute it and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation, either version
  * 3 of the License, or (at your option) any later version.
  *
  * OpenShot Audio Library (libopenshot-audio) is distributed in the hope that
  * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with OpenShot Library. If not, see <http://www.gnu.org/licenses/>.
  *
  * @mainpage OpenShot Audio Library C++ API
