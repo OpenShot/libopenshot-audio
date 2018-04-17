ff2a44ad84de03a904e56211a25f8b455a1b45a8
Richard Simpson <richardsimpson@outlook.com>
2018-04-17 00:16:07 -0500

    Remove unused modules and modernize CMake.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -17,27 +17,22 @@
 #include "modules/juce_audio_basics/juce_audio_basics.h"
 #include "modules/juce_audio_devices/juce_audio_devices.h"
 #include "modules/juce_audio_formats/juce_audio_formats.h"
-#include "modules/juce_audio_processors/juce_audio_processors.h"
 #include "modules/juce_core/juce_core.h"
-#include "modules/juce_data_structures/juce_data_structures.h"
 #include "modules/juce_events/juce_events.h"
-#include "modules/juce_graphics/juce_graphics.h"
-#include "modules/juce_gui_basics/juce_gui_basics.h"
-#include "modules/juce_gui_extra/juce_gui_extra.h"
 
-#if ! DONT_SET_USING_JUCE_NAMESPACE
- // If your code uses a lot of JUCE classes, then this will obviously save you
- // a lot of typing, but can be disabled by setting DONT_SET_USING_JUCE_NAMESPACE.
- using namespace juce;
+#if !DONT_SET_USING_JUCE_NAMESPACE
+// If your code uses a lot of JUCE classes, then this will obviously save you
+// a lot of typing, but can be disabled by setting DONT_SET_USING_JUCE_NAMESPACE.
+using namespace juce;
 #endif
 
-#if ! JUCE_DONT_DECLARE_PROJECTINFO
+#if !JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "libopenshot-audio";
-    const char* const  versionString  = "0.0.1";
-    const int          versionNumber  = 0x1;
+const char *const projectName = "libopenshot-audio";
+const char *const versionString = "0.0.1";
+const int versionNumber = 0x1;
 }
 #endif
 
-#endif   // __APPHEADERFILE_IIUU5J__
+#endif // __APPHEADERFILE_IIUU5J__
