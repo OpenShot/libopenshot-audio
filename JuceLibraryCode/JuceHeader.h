10835cb6be4c08b637356e23ec517bbda6d78342
Jonathan Thomas <jonathan@openshot.org>
2019-03-30 14:41:08 -0500

    Experimental attempt to ignore MacType.h Point class

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -14,13 +14,14 @@
 
 #include "AppConfig.h"
 
+#define Point IgnorePoint
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_core/juce_core.h>
 #include <juce_data_structures/juce_data_structures.h>
 #include <juce_events/juce_events.h>
-
+#undef Point
 
 #if ! DONT_SET_USING_JUCE_NAMESPACE
  // If your code uses a lot of JUCE classes, then this will obviously save you
